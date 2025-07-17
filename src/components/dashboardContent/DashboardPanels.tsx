621edab361c371ca9154ba3e73694d426ef60868
AhmedEssam <ae.devverse@gmail.com>
2025-07-17 17:18:04 +0300

    Feat: Create DashboardPanels component that wraps all panels

---
+++ src/components/dashboardContent/DashboardPanels.tsx
@@ -0,0 +1,21 @@
+import LatestFilesPanel from "./LatestFilesPanel";
+import RoadmapPanel from "./RoadmapPanel";
+import SpendingsPanel from "./SpendingsPanel";
+import SummaryPanel from "./SummaryPanel";
+
+function DashboardPanels() {
+  return (
+    <section className="space-y-xl">
+      <SummaryPanel />
+
+      <div className="grid grid-cols-1 lg:grid-cols-2 gap-xl">
+        <RoadmapPanel />
+        <LatestFilesPanel />
+      </div>
+
+      <SpendingsPanel />
+    </section>
+  );
+}
+
+export default DashboardPanels;
