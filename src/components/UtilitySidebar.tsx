450c89a5798f5e3226256c8810666c6062d5a4e0
AhmedEssam <ae.devverse@gmail.com>
2025-07-17 21:41:24 +0300

    Move to components folder

---
+++ src/components/UtilitySidebar.tsx
@@ -0,0 +1,26 @@
+import { useUtilitiesPanel } from "../contexts/UtilitiesPanelContext";
+import Activities from "./utilitiesWrapper/Activities";
+import Contacts from "./utilitiesWrapper/Contacts";
+import Notifications from "./utilitiesWrapper/Notifications";
+
+function UtilitySidebar() {
+  const { isVisible } = useUtilitiesPanel();
+
+  if (!isVisible) return null;
+
+  return (
+    <aside
+      className={`flex flex-col gap-narrow h-full p-md border-s border-s-dark-faint dark:border-s-light-soft ${
+        isVisible ? "opacity-100" : "opacity-0 pointer-events-none"
+      }`}
+    >
+      <Notifications />
+
+      <Activities />
+
+      <Contacts />
+    </aside>
+  );
+}
+
+export default UtilitySidebar;
