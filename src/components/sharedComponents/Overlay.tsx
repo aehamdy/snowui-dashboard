12d2150473fb01c020a0a1403cafde52f832c188
AhmedEssam <ae.devverse@gmail.com>
2025-07-18 21:05:55 +0300

    Feat: Isolate the overlay code part from LayoutGrid and make it as a reusable component

---
+++ src/components/sharedComponents/Overlay.tsx
@@ -0,0 +1,26 @@
+import { useSearchPanel } from "../../hooks/useSearchPane.l";
+import { useSidebar } from "../../hooks/useSidebar";
+import { useUtilitiesPanel } from "../../hooks/useUtilitiesPanel";
+
+function Overlay() {
+  const { isVisible: isSidebarVisible, toggle: toggleSidebar } = useSidebar();
+  const { isVisible: isUtilityVisible, toggle: toggleUtilities } =
+    useUtilitiesPanel();
+  const { isSearchPanelOpen, closeSearchPanel } = useSearchPanel();
+
+  return (
+    (isSidebarVisible || isUtilityVisible || isSearchPanelOpen) && (
+      <div
+        onClick={() => {
+          if (isSidebarVisible) toggleSidebar();
+          if (isUtilityVisible) toggleUtilities();
+          if (isSearchPanelOpen) closeSearchPanel();
+        }}
+        className={`${
+          isSearchPanelOpen ? "lg:block" : "lg:hidden"
+        } absolute top-0 start-0 w-full h-full bg-dark-secondary z-30 transition-opacity duration-300 opacity-80`}
+      ></div>
+    )
+  );
+}
+export default Overlay;
