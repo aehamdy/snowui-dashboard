189a8941765a4b2f0bdbcc19ca4e8c42e578dfb5
AhmedEssam <ae.devverse@gmail.com>
2025-07-18 05:10:00 +0300

    Feat: Make Sidebar and UtilitySidebar collabsible upon click on their buttons

--- src/layout/LayoutGrid.tsx
+++ src/layout/LayoutGrid.tsx
@@ -3,6 +3,7 @@ import { useUtilitiesPanel } from "../contexts/UtilitiesPanelContext";
 import Sidebar from "../components/Sidebar";
 import DashboardContent from "../components/DashboardContent";
 import UtilitySidebar from "../components/UtilitySidebar";
+import { useEffect } from "react";
 
 function LayoutGrid() {
   const { isVisible: isSidebarVisible } = useSidebar();
@@ -12,23 +13,45 @@ function LayoutGrid() {
   const utilityColSpan = isUtilityVisible ? 2 : 0;
   const dashboardColSpan = 12 - sidebarColSpan - utilityColSpan;
 
+  console.log({
+    sidebar: sidebarColSpan,
+    utility: utilityColSpan,
+    dashboard: dashboardColSpan,
+  });
+
   const getDashboardColClass = () => {
     switch (dashboardColSpan) {
       case 12:
         return "col-span-12";
       case 10:
-        return "col-span-10";
+        return "col-span-12 lg:col-span-10";
       case 8:
-        return "col-span-8";
+        return "col-span-12 lg:col-span-8";
       default:
         return "col-span-12";
     }
   };
 
+  useEffect(() => {
+    const isSmallScreen = window.innerWidth < 1024;
+
+    if (isSmallScreen && (isSidebarVisible || isUtilityVisible)) {
+      document.body.classList.add("overflow-hidden");
+    } else {
+      document.body.classList.remove("overflow-hidden");
+    }
+
+    return () => {
+      document.body.classList.remove("overflow-hidden");
+    };
+  }, [isSidebarVisible, isUtilityVisible]);
+
   return (
     <main className="relative grid grid-cols-12 h-dvh text-small font-regular text-dark-primary dark:text-light-primary transition-all duration-300">
       {isSidebarVisible && (
-        <div className="col-span-2 h-full absolute start-0 lg:relative bg-light-primary dark:bg-[#2a2a2a] overflow-hidden transition-all duration-300 z-20 lg:z-0">
+        <div
+          className={`absolute lg:relative col-span-2 h-full top-0 start-0 bg-light-primary dark:bg-[#2a2a2a] z-40 lg:z-0`}
+        >
           <Sidebar />
         </div>
       )}
@@ -40,7 +63,7 @@ function LayoutGrid() {
       </div>
 
       {isUtilityVisible && (
-        <div className="col-span-2 h-full absolute end-0 lg:relative bg-light-primary dark:bg-[#2a2a2a] overflow-hidden transition-all duration-300 z-20 lg:z-0">
+        <div className="absolute lg:relative col-span-2 h-full top-0 end-0 bg-light-primary dark:bg-[#2a2a2a] z-40 lg:z-0">
           <UtilitySidebar />
         </div>
       )}
