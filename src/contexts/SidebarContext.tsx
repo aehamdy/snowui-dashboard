108defcb1ca154dd1f7b608cefa6ec10cf38449b
AhmedEssam <ae.devverse@gmail.com>
2025-08-01 15:39:15 +0300

    Chore: Add closeSidebar type

--- src/contexts/SidebarContext.tsx
+++ src/contexts/SidebarContext.tsx
@@ -3,6 +3,7 @@ import { createContext } from "react";
 type SidebarContextType = {
   isVisible: boolean;
   toggle: () => void;
+  closeSidebar: () => void;
 };
 
 export const SidebarContext = createContext<SidebarContextType | undefined>(
